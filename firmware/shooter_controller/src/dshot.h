9abc671e06df4fdc8687101c1927f9d22f33b162
Julian Ho <julianho@brandeis.edu>
2020-12-03 20:26:21 +0800

    Add encoder support

--- firmware/shooter_controller/src/dshot.h
+++ firmware/shooter_controller/src/dshot.h
@@ -12,49 +12,6 @@ typedef struct
 	volatile uint16_t throttle;
 } dshot_t;
 
-void dshotOutput(uint16_t value, rmt_data_t *dshot_packet, rmt_obj_t *rmt_send)
-{
-	uint16_t packet;
-
-	// telemetry bit
-	packet = (value << 1) | 0;
-
-	// https://github.com/betaflight/betaflight/blob/09b52975fbd8f6fcccb22228745d1548b8c3daab/src/main/drivers/pwm_output.c#L523
-	int csum = 0;
-	int csum_data = packet;
-	for (int i = 0; i < 3; i++)
-	{
-		csum ^= csum_data;
-		csum_data >>= 4;
-	}
-	csum &= 0xf;
-	packet = (packet << 4) | csum;
-
-	// durations are for dshot600
-	// https://blck.mn/2016/11/dshot-the-new-kid-on-the-block/
-	// Bit length (total timing period) is 1.67 microseconds (T0H + T0L or T1H + T1L).
-	// For a bit to be 1, the pulse width is 1250 nanoseconds (T1H – time the pulse is high for a bit value of ONE)
-	// For a bit to be 0, the pulse width is 625 nanoseconds (T0H – time the pulse is high for a bit value of ZERO)
-	for (int i = 0; i < 16; i++)
-	{
-		if (packet & 0x8000)
-		{
-			dshot_packet[i].level0 = 1;
-			dshot_packet[i].duration0 = 100;
-			dshot_packet[i].level1 = 0;
-			dshot_packet[i].duration1 = 34;
-		}
-		else
-		{
-			dshot_packet[i].level0 = 1;
-			dshot_packet[i].duration0 = 50;
-			dshot_packet[i].level1 = 0;
-			dshot_packet[i].duration1 = 84;
-		}
-		packet <<= 1;
-	}
-
-	rmtWrite(rmt_send, dshot_packet, 16);
-
-	return;
-}
+void createMotor(int pin, dshot_t *motor);
+void setMotorThrottle(uint16_t throttle, dshot_t *motor);
+void dshotOutput(uint16_t value, rmt_data_t *dshot_packet, rmt_obj_t *rmt_send);
--- firmware/shooter_controller/src/main.cpp
+++ firmware/shooter_controller/src/main.cpp
@@ -1,41 +1,14 @@
 #include <Arduino.h>
 #include "dshot.h"
 
-#define MOTOR_0 0
-#define MOTOR_1 1
-
 hw_timer_t *timer = NULL;
-dshot_t motors[2];
+dshot_t motor_0;
+dshot_t motor_1;
 
 void IRAM_ATTR onTimer()
 {
-	dshotOutput(motors[MOTOR_0].throttle, motors[MOTOR_0].dshot_packet, motors[MOTOR_0].rmt_send);
-	dshotOutput(motors[MOTOR_1].throttle, motors[MOTOR_1].dshot_packet, motors[MOTOR_1].rmt_send);
-}
-
-void createMotor(int pin, dshot_t *motor)
-{
-	rmt_obj_t *rmt_send;
-	if ((rmt_send = rmtInit(pin, true, RMT_MEM_64)) == NULL)
-	{
-		Serial.println("init sender failed\n");
-		return;
-	}
-
-	rmtSetTick(rmt_send, 12.5);
-
-	motor->rmt_send = rmt_send;
-	motor->throttle = DSHOT_IDLE;
-}
-
-void setMotorThrottle(int id, uint16_t throttle)
-{
-	if (throttle > 2000 || throttle < 0)
-	{
-		return;
-	}
-
-	motors[id].throttle = throttle + DSHOT_MIN;
+	dshotOutput(motor_0.throttle, motor_0.dshot_packet, motor_0.rmt_send);
+	dshotOutput(motor_1.throttle, motor_1.dshot_packet, motor_1.rmt_send);
 }
 
 void setup()
@@ -44,8 +17,8 @@ void setup()
 	Serial.println("init starting");
 
 	// Motors init
-	createMotor(0, &motors[0]);
-	createMotor(2, &motors[1]);
+	createMotor(0, &motor_0);
+	createMotor(2, &motor_1);
 
 	// Timer init, prescaler 80, divider 500 => 2khz
 	timer = timerBegin(0, 80, true);
@@ -55,23 +28,24 @@ void setup()
 
 	// ESC init
 	vTaskDelay(500 / portTICK_PERIOD_MS);
-	setMotorThrottle(MOTOR_0, 0);
-	setMotorThrottle(MOTOR_1, 0);
+	setMotorThrottle(0, &motor_0);
+	setMotorThrottle(0, &motor_1);
 }
 
 void loop()
 {
 	int throttle = Serial.parseInt();
-	if (throttle > 0)
-	{
-		Serial.printf("val: %d\n", throttle);
-		setMotorThrottle(MOTOR_0, throttle);
-		setMotorThrottle(MOTOR_1, throttle);
-	}
-	else if (throttle == -1)
+
+	if (throttle == 9999)
 	{
 		Serial.println("stop");
-		setMotorThrottle(MOTOR_0, 0);
-		setMotorThrottle(MOTOR_1, 0);
+		setMotorThrottle(0, &motor_0);
+		setMotorThrottle(0, &motor_1);
+	}
+	else if (throttle > 0)
+	{
+		Serial.printf("val: %d\n", throttle);
+		setMotorThrottle(throttle, &motor_0);
+		setMotorThrottle(throttle, &motor_1);
 	}
 }
