4c995c4277a9189fcee95506b934256e35c9d99c
Julian Ho <sscj2010@gmail.com>
2020-12-12 12:54:06 +0800

    Share fixed variables instead of hardcoded values

--- lib/quickshot/quickshot.py
+++ lib/quickshot/quickshot.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Shooter(object):
     """
     QuickShot library provides easy function for calculating shooter parameters based
@@ -8,13 +11,18 @@ class Shooter(object):
     max_rpm = 0
     min_angle = 0
     max_angle = 0
+    roller_circum = 0
+    roller_circum = 0
 
-    def __init__(self, min_rpm, max_rpm, min_angle, max_angle):
+    def __init__(
+        self, min_rpm, max_rpm, min_angle, max_angle, roller_circum=9, roller_dist=5.6
+    ):
         self.min_rpm = min_rpm
         self.max_rpm = max_rpm
         self.min_angle = min_angle
         self.max_angle = max_angle
-
+        self.roller_circum = roller_circum
+        self.roller_dist = roller_dist
 
     def calc_params(self, y, speed, top_spin=0, back_spin=0):
         """
@@ -32,43 +40,28 @@ class Shooter(object):
         Returns:
             params: The parameters to be used for the robot.
         """
-        top_rpm = 0
-        bottom_rpm = 0
         angle = 0
 
-        return {"top_rpm": top_rpm, "bottom_rpm": bottom_rpm, "angle": angle}
-    
-    ## The actual code
-    import math
-class shooter():
-    roller1_rpm = 0
-    roller2_rpm = 0
-    min_angle = 0
-    max_angle = 0
+        time = y / speed
+        y_speed = time * 9.8
+        angle = math.degrees(math.atan(y_speed / speed))
+        launch_speed = math.sqrt(y_speed * y_speed + speed * speed)
 
-    def __init__(self, roller1_rpm, roller2_rpm, min_angle, max_angle):
-        self.roller1_rpm = roller1_rpm
-        self.roller2_rpm = roller2_rpm
-        self.min_angle = min_angle
-        self.max_angle = max_angle
-
-    def calc_params(self, location, speed, top_spin, back_spin):
-        time=location/speed
-        y_speed=time*9.8
-        angle=math.degrees(math.atan(y_speed/speed))
-        launch_speed=math.sqrt(y_speed*y_speed+speed*speed)
-
-        if top_spin==0 and back_spin==0:
-            rpm=launch_speed/9*math.pi #circumference of the roller
-            roller1_rpm=rpm
-            roller2_rpm=rpm
-        elif top_spin>0:
-            ball_rpm=top_spin/5.6*math.pi
-            roller1_rpm=(launch_speed-ball_rpm*5.6)/9
-            roller2_rpm=(2*launch_speed/9)-roller1_rpm
+        if top_spin == 0 and back_spin == 0:
+            rpm = launch_speed / self.roller_circum * math.pi
+            roller1_rpm = rpm
+            roller2_rpm = rpm
+        elif top_spin > 0:
+            ball_rpm = top_spin / self.roller_dist * math.pi
+            roller1_rpm = (
+                launch_speed - ball_rpm * self.roller_dist
+            ) / self.roller_circum
+            roller2_rpm = (2 * launch_speed / self.roller_circum) - roller1_rpm
         else:
-            ball_rpm=top_spin/5.6*math.pi
-            roller2_rpm=(launch_speed-ball_rpm*5.6)/9
-            roller1_rpm=(2*launch_speed/9)-roller2_rpm
-        
-        max_angle=angle
+            ball_rpm = top_spin / self.roller_dist * math.pi
+            roller2_rpm = (
+                launch_speed - ball_rpm * self.roller_dist
+            ) / self.roller_circum
+            roller1_rpm = (2 * launch_speed / self.roller_circum) - roller2_rpm
+
+        return {"top_rpm": roller1_rpm, "bottom_rpm": roller2_rpm, "angle": angle}
